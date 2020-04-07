878ac1a526badb3b45179b9cd07e103770b73271
Simon Spielmann <simon.spielmann@capgemini.com>
2020-04-07 08:40:39 +0200

    Rework test

--- core/src/test/java/com/employee/employeemanagement/service/impl/rest/EmployeeManagementKafkaServiceImplTest.java
+++ core/src/test/java/com/employee/employeemanagement/service/impl/rest/EmployeeManagementKafkaServiceImplTest.java
@@ -1,7 +1,6 @@
 package com.employee.employeemanagement.service.impl.rest;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
+import java.time.Duration;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.awaitility.Awaitility;
@@ -9,10 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.devonfw.module.kafka.common.messaging.retry.api.RetryState;
 import com.devonfw.module.service.common.api.client.config.ServiceClientConfigBuilder;
 import com.employee.employeemanagement.logic.api.to.EmployeeEto;
 import com.employee.employeemanagement.logic.api.to.EmployeeSearchCriteriaTo;
@@ -33,14 +30,13 @@ public class EmployeeManagementKafkaServiceImplTest extends RestServiceTest {
   /** Logger instance. */
   private static final Logger LOG = LoggerFactory.getLogger(EmployeeManagementKafkaServiceImplTest.class);
 
-  private ProducerRecord<Object, Object> producerRecord;
-
   /**
    *
    */
   @Test
-  public void saveEmployeeViaKafkaService() {
+  public void saveAndDeleteEmployeeViaKafkaService() {
 
+    // Step 1: Add new employee via Kafka
     // Arrange
     EmployeeEto employee = new EmployeeEto();
     employee.setAge(24);
@@ -55,87 +51,32 @@ public class EmployeeManagementKafkaServiceImplTest extends RestServiceTest {
       LOG.error("Error while converting employee as String");
     }
 
-    this.producerRecord = new ProducerRecord<>("sample-employee-topic", 0, "employee", convertedMessage);
-
-    setHeaders(this.producerRecord);
+    ProducerRecord<String, String> producerRecord = new ProducerRecord<>("employeeapp-employee-v1-add",
+        convertedMessage);
 
     EmployeemanagementRestService employeemanagementRestService = getServiceClientFactory()
         .create(EmployeemanagementRestService.class, new ServiceClientConfigBuilder().host("localhost").authBasic()
             .userLogin("manager").userPassword("manager").buildMap());
 
     // Act
-    getMessageSender().sendMessage(this.producerRecord);
+    getMessageSender().sendMessage(producerRecord);
 
     // Assert
     EmployeeSearchCriteriaTo employeCriteria = new EmployeeSearchCriteriaTo();
     employeCriteria.setName(employee.getName());
     employeCriteria.setLocation(employee.getLocation());
 
-    Page<EmployeeEto> fetchedEmployee = employeemanagementRestService.findEmployees(employeCriteria);
+    Awaitility.await().atMost(Duration.ofMillis(3000))
+        .until(() -> employeemanagementRestService.findEmployees(employeCriteria).getTotalElements() == 1);
 
-    Awaitility.await().until(() -> fetchedEmployee.isEmpty() == false);
-
-  }
-
-  /**
-  *
-  */
-  @Test
-  public void deleteEmployeeViaKafkaService() {
-
-    // Arrange
-    EmployeeEto employee = new EmployeeEto();
-    employee.setAge(24);
-    employee.setLocation("Chennai,India");
-    employee.setName("Ashwin");
-    employee.setValidEmployee(false);
-
-    EmployeemanagementRestService employeemanagementRestService = getServiceClientFactory()
-        .create(EmployeemanagementRestService.class, new ServiceClientConfigBuilder().host("localhost").authBasic()
-            .userLogin("manager").userPassword("manager").buildMap());
-
-    employeemanagementRestService.saveEmployee(employee);
-
-    EmployeeSearchCriteriaTo employeCriteria = new EmployeeSearchCriteriaTo();
-    employeCriteria.setName(employee.getName());
-    employeCriteria.setLocation(employee.getLocation());
-
-    Page<EmployeeEto> fetchedEmployee = employeemanagementRestService.findEmployees(employeCriteria);
-
-    ProducerRecord<String, String> record = new ProducerRecord<>("delete-Employee", 0, "employeeId",
-        fetchedEmployee.getContent().get(0).getId().toString());
-
-    setRetryHeaders(record);
+    // Step 2: Delete employee via Kafka
+    EmployeeEto newEmployee = employeemanagementRestService.findEmployees(employeCriteria).getContent().get(0);
+    producerRecord = new ProducerRecord<>("employeeapp-employee-v1-delete", newEmployee.getId().toString());
 
     // Act
-    getMessageSender().sendMessage(record);
-
+    getMessageSender().sendMessage(producerRecord);
     // Assert
     Awaitility.await().until(() -> employeemanagementRestService.findEmployees(employeCriteria).isEmpty() == true);
-
-  }
-
-  private <K, V> void setHeaders(ProducerRecord<K, V> producerRecord) {
-
-    // can add more custom headers
-
-    // Retry headers
-    setRetryHeaders(producerRecord);
-  }
-
-  private <K, V> void setRetryHeaders(ProducerRecord<K, V> producerRecord) {
-
-    Instant today = Instant.now();
-
-    // value for the headers.
-    getMessageRetryContext().setRetryCount(2);
-    getMessageRetryContext().setRetryNext(Instant.parse(today.toString()));
-    getMessageRetryContext().setRetryReadCount(1);
-    getMessageRetryContext().setRetryUntil(Instant.parse(today.plus(15, ChronoUnit.DAYS).toString()));
-    getMessageRetryContext().setRetryState(RetryState.PENDING);
-
-    // injecting retry headers.
-    getMessageRetryContext().injectInto(producerRecord);
   }
 
 }
